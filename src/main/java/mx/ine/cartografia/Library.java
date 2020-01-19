0b0b1756d83957d94ad97ab10adf6f6cf42f8732
Joel <joelmmx@gmail.com>
2020-01-19 01:44:27 -0600

    With conection to Data Base

--- src/main/java/mx/ine/cartografia/Library.java
+++ src/main/java/mx/ine/cartografia/Library.java
@@ -1,5 +1,12 @@
 package mx.ine.cartografia;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -7,8 +14,27 @@ import javax.ws.rs.Path;
 public class Library {
 	@GET
 	@Path("/books")
-	public String getBooks() {
+	public String getBooks() throws Exception{
 		System.out.println("Hola");
-		return "Hola";
+		return "Hola: "+getDataSource();
+	}
+	
+	private String getDataSource() throws Exception{
+		// Obtain our environment naming context
+		Context initCtx = new InitialContext();
+		//Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+		// Look up our data source
+		DataSource ds = (DataSource)
+		  initCtx.lookup("java:jboss/datasources/DataSourceDS");
+
+		// Allocate and use a connection from the pool
+		Connection conn = ds.getConnection();
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("select * from test_table");
+		rs.next();
+		String result = rs.getString("name");
+		conn.close();
+		return result;
 	}
 }
